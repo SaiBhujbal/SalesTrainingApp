57625fa951959dc8975398bdc50e5e05e7892fba
Slayer <46700402+SaiBhujbal@users.noreply.github.com>
2024-11-15 23:01:13 +0530

    Update ContinueConversation.py

--- ContinueConversation.py
+++ ContinueConversation.py
@@ -14,7 +14,7 @@ products_table = dynamodb.Table('Products')
 persona_progress_table = dynamodb.Table('PersonaProgress')
 
 # Define the SageMaker endpoint
-ENDPOINT_NAME = 'jumpstart-dft-hf-llm-nvidia-llama3-20241115-072526'  # Replace with your actual endpoint name
+ENDPOINT_NAME = 'sagemaker-endpoint-name'  # Replace with your actual endpoint name
 
 def lambda_handler(event, context):
     # Logging the received event for debugging
